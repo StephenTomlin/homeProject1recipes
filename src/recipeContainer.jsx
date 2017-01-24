63b0f7dd4ae3b5483a18197cc21c803df9a40543
StephenTomlin <Stephen.Tomlinson@live.com>
2017-01-24 12:52:18 -0500

    changed background color

--- src/recipeContainer.jsx
+++ src/recipeContainer.jsx
@@ -1,14 +1,6 @@
 import React, {Component} from 'react';
 
 class Recipes extends Component {
-  componentDidMount() {
-    $.ajax({
-      url: 'http://api.yummly.com/v1/api/recipe/Avocado-cream-pasta-sauce-recipe-306039?_app_id=448f67d9&_app_key=c21a694ca9204f51fa82d8ade53c791b',
-      data: data,
-      success: function() {this.setstate,
-      dataType: dataType
-    });
-  }
   render() {
     return (
       <div>
