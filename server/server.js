513c3847fd5b0e6910be605f64fccf8343a79647
StephenTomlin <Stephen.Tomlinson@live.com>
2017-01-24 13:24:35 -0500

    some progrss on the backend

---
+++ server/server.js
@@ -0,0 +1,14 @@
+"use strict";
+
+require('dotenv').config();
+
+const PORT        = process.env.PORT || 8080;
+const ENV         = process.env.ENV || "development";
+const express     = require("express");
+const app         = express();
+
+const recipeSearchRoute = require("./routes/recipeSearch.js")
+
+app.listen(PORT, () => {
+  console.log("Example app listening on port " + PORT);
+});
--- src/navBar.jsx
+++ src/navBar.jsx
@@ -1,11 +1,34 @@
 import React, {Component} from 'react';
 
 class Navbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    if (this.state.value != '') {
+      fetch('http://localhost:8080/api/')
+    }
+  }
+
   render() {
     return (
-      <div>
-        <h1>Mom's cookbook directory</h1>
-      </div>
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Search for Recipes
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
     );
   }
 }
