718de864e58dfaae1ede5251e6f2f76ded0fadd8
StephenTomlin <Stephen.Tomlinson@live.com>
2017-01-25 20:30:51 -0500

    added some styling

--- src/recipeContainer.jsx
+++ src/recipeContainer.jsx
@@ -9,12 +9,12 @@ class Recipes extends Component {
       <section className="contain-recipes">
       {this.props.recipeList.map((item,index)=>(
         <div className ="rowStyle" value={item} key={index}>
-          <div>
-            <h3>{item.recipeName}</h3>
-            <h4>{item.totalTimeInSeconds}</h4>
-            <img src={item.imageUrlsBySize[90]} height="90" width="90"/>
+          <div className="recipeCard">
+            <h3 className="cardContents">{item.recipeName}</h3>
+            <h4 className="cardContents">{item.totalTimeInSeconds}</h4>
+            <img className="cardContents" src={item.imageUrlsBySize[90]}/>
             {item.ingredients.map(function (value) {
-              return <p key={value}>{value}</p>;
+              return <p className="cardContents" key={value}>{value}</p>;
             })}
           </div>
         </div>
