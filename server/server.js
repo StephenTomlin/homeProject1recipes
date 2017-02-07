6d318213e4a1f1269013d15616fc4e3121f053cf
StephenTomlin <Stephen.Tomlinson@live.com>
2017-02-07 11:44:40 -0500

    merging

--- server/server.js
+++ server/server.js
@@ -4,15 +4,27 @@ require('dotenv').config();
 
 const PORT        = process.env.PORT || 8080;
 const ENV         = process.env.ENV || "development";
-const express     = require("express");
+const express     = require('express');
 const app         = express();
 
-const knexConfig = require("./knexfile");
-const knex       = require("knex")(knexConfig[ENV]);
+const knexConfig = require('./knexfile');
+const knex       = require('knex')(knexConfig[ENV]);
+const morgan     = require('morgan')
 const knexLogger = require('knex-logger');
 
 const recipeSearchRoute = require("./routes/recipe.js");
 const recipeSaveRoute   = require("./routes/recipeSave.js");
+
+// Load requirements
+const http = require('http')
+
+// Load the logger first so all (static) HTTP requests are logged to STDOUT
+// 'dev' = Concise output colored by response status for development use.
+//         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
+app.use(morgan('dev'));
+
+// Log knex SQL queries to STDOUT as well
+app.use(knexLogger(knex));
 
 app.use(function(req,res,next) {
   res.header("Access-Control-Allow-Origin", req.headers.origin);
