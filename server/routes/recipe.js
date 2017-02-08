715581b909424307c354ffd1f2f1c9e5ba4bde69
Clive Yip <cliveyip@Clives-MacBook-Pro.local>
2017-02-07 19:27:16 -0500

    tidy up states and event handlers

--- server/routes/recipe.js
+++ server/routes/recipe.js
@@ -5,14 +5,32 @@ const router  = express.Router();
 
 
 router.get('/', function(req, res, next) {
-  console.log(req.headers.searchparams)
-  request(`http://api.yummly.com/v1/api/recipes?_app_id=448f67d9&_app_key=c21a694ca9204f51fa82d8ade53c791b&q=${req.headers.searchparams}&requirePictures=true`, function(error, response, body) {
+  console.log(req.headers.SearchParams)
+  // console.log(req.headers.SearchParams.query)
+  var app_id = "448f67d9"
+  var app_key = "c21a694ca9204f51fa82d8ade53c791b"
+  var url = "http://api.yummly.com/v1/api/recipes"
+  var url_full = url + "?" + "_app_id=" + app_id + "&" + "_app_key=" + app_key
+                      + "&q=" + req.headers.SearchParams['query']
+                      + "&excludedIngredient[]=" + req.headers.SearchParams['excludedIngredient']
+
+  request(url_full, function(error, response, body) {
     if (!error && response.statusCode === 200) {
-      console.log(body);
+      // console.log(body);
       res.json(body);
     } else {
       res.json(error)
     }
   })
+
+
+  // request(`http://api.yummly.com/v1/api/recipes?_app_id=448f67d9&_app_key=c21a694ca9204f51fa82d8ade53c791b&q=${req.headers.searchparams}&requirePictures=true&excludedIngredient[]=${req.headers.excludedIngredientParams}`, function(error, response, body) {
+  //   if (!error && response.statusCode === 200) {
+  //     console.log(body);
+  //     res.json(body);
+  //   } else {
+  //     res.json(error)
+  //   }
+  // })
 });
 module.exports = router;
--- src/App.jsx
+++ src/App.jsx
@@ -6,57 +6,39 @@ import { Button, Form, FormGroup, FormControl, Col, ControlLabel, Checkbox, Labe
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {value: '',
+    this.state = {query: '',
+                  excludedIngredient: '',
                   recipes:[],
-                  available:[],
-                  availableRecipes:[],
-                  availableCourses:[],
-                  selectedCourses:[],
-                  keywordList: ['egg', 'ham', 'pepper']
+                  availableRecipes: []
                 };
 
-    this.handleChange = this.handleChange.bind(this);
+    this.handleQueryChange = this.handleQueryChange.bind(this);
+    this.handleExcludedIngredientChange = this.handleExcludedIngredientChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleQuery = this.handleQuery.bind(this);
   }
 
-  handleChange(event) {
-    // var keywordList = this.state.keywordList.slice()
-    // keywordList.push(event.target.value)
-    // this.setState({keywordList: keywordList })
-    this.setState({value: event.target.value});
+  handleQueryChange(event) {
+    this.setState({query: event.target.value});
   }
 
-  handleEnter(event) {
-    console.log(event.target.value);
-  }
-
-  handleQuery(event){
-    if (this.state.value != '') {
-      fetch('http://localhost:8080/api/recipes', {
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json',
-          'SearchParams': this.state.value
-        }
-      })
-      .then((response) => response.json())
-      .then((responseJson) => {
-        let parsed = JSON.parse(responseJson)
-        this.setState({recipes: parsed.matches})
-        console.log(this.state.recipes)
-        })
-    }
+  handleExcludedIngredientChange(event) {
+    this.setState({excludedIngredient: event.target.value});
   }
 
   handleSubmit(event) {
     event.preventDefault();
-    if (this.state.value != '') {
+    if (this.state.query != '') {
       fetch('http://localhost:8080/api/recipes', {
         headers: {
           'Accept': 'application/json',
           'Content-Type': 'application/json',
-          'SearchParams': this.state.value
+          'SearchParams':
+            {
+              'query': this.state.query,
+              'excludedIngredient': this.state.excludedIngredient
+            }
+          // 'SearchParams': this.state.query,
+          // 'excludedIngredientParams': this.state.excludedIngredient
         }
       })
       .then((response) => response.json())
@@ -66,39 +48,28 @@ class App extends Component {
         console.log(this.state.recipes)
         })
     }
-    this.setState({keywordList: this.state.keywordList.concat([event.target.value]) })
     console.log("Event.target.value :", event.target.value);
     console.log("Event.target :", event.target);
     console.log("Event :", event);
   }
 
-  componentDidMount() {
-    this.setState(
-      {availableCourses:
-        ["Main Dishes", "Desserts", "Side Dishes", "Lunch and Snacks", "Appetizers", "Salads",
-        "Breads", "Breakfast and Brunch", "Soups", "Beverages", "Condiments and Sauces", "Cocktails"]
-      }
-    );
-  }
-
-
   render() {
     return (
       <div className = "container">
         <div className = "row">
-          Enter search criteria
+          <h3> Enter search criteria </h3>
         </div>
         <div className = "row">
           <Form horizontal onSubmit={this.handleSubmit}>
-            <FormGroup controlId="formHorizontalRecipeName">
+            <FormGroup controlId="queryBar">
               <Col componentClass={ControlLabel} sm={2}>
                 Recipe name contains:
               </Col>
               <Col sm={4}>
-                <FormControl type="text" placeholder="Example: omelette" onChange={this.handleChange} />
+                <FormControl type="text" placeholder="Example: omelette" onChange={this.handleQueryChange} />
               </Col>
             </FormGroup>
-            <FormGroup controlId="formHorizontalIngredientsContain">
+            <FormGroup controlId="allowedIngredientBar">
               <Col componentClass={ControlLabel} sm={2}>
                 Ingredients contains:
               </Col>
@@ -106,12 +77,12 @@ class App extends Component {
                 <FormControl type="text" placeholder="Example: egg pepper salt" />
               </Col>
             </FormGroup>
-            <FormGroup controlId="formHorizontalIngredientsDoesNotContain">
+            <FormGroup controlId="excludedIngredientBar">
               <Col componentClass={ControlLabel} sm={2}>
                 Ingredients does NOT contain:
               </Col>
               <Col sm={4}>
-                <FormControl type="text" placeholder="Example: onions" />
+                <FormControl type="text" placeholder="Example: onions" onChange={this.handleExcludedIngredientChange}/>
               </Col>
             </FormGroup>
 
